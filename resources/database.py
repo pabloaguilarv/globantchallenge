8065ffb027272ff82a166ecda4283485971fc618
Agui <baster94@hotmail.com>
2024-11-17 20:25:43 -0500

    removed secrets

--- resources/database.py
+++ resources/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_PATH = 'postgresql://postgres:Sez7dllxPA@localhost:5432/challenge'
-SQLALCHEMY_DATABASE_PATH = 'postgresql+psycopg2://avnadmin:AVNS_9V67I1GWUPLcMF93Bhh@challenge-challenge-globant-3b2c.k.aivencloud.com:25318/challenge?sslmode=require'
+SQLALCHEMY_DATABASE_PATH = f'postgresql+psycopg2://avnadmin:{os.environ.get('password')}@challenge-challenge-globant-3b2c.k.aivencloud.com:25318/challenge?sslmode=require'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_PATH,
